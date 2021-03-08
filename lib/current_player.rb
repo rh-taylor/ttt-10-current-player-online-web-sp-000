1b2a3ff2d7a9576e7f857908ba4bbc6a6f100674
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-08 18:04:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,15 +7,3 @@ def turn_count(board)
     end
   counter
 end
-
-
-
-def turn_count(board)
-   counter = 0
-   board.each do |spaces|
-      if spaces == "X" || spaces == "O"
-         counter += 1
-      end
-   end
-   counter
-end
